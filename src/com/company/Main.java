95fad9cb52af274f79f5c0b7abc2e29d2ead0a91
katrich1996 <sergey.katrich96@mail.ru>
2022-03-29 18:30:44 +0300

    Test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,19 @@
 package com.company;
 
 public class Main {
-
+///task1
     public static void main(String[] args) {
-	// write your code here
+	byte type1 = 126;
+        System.out.println(type1);
+        short type2 = 10000;
+        System.out.println(type2);
+        int type3 = 777;
+        System.out.println(type3);
+        long type4 = 1867L;
+        System.out.println(type4);
+        float type5 = 16.5F;
+        System.out.println(type5);
+        double type6 = 18.5;
+        System.out.println(type6);
     }
 }
